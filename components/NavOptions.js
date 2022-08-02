28c7ba11d174902fdf4065efedda89c75d24f0fa
kblatsos <kblatsos@infili.com>
2022-08-02 12:11:26 +0300

    navigation done and installed google-places

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -19,13 +20,17 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
@@ -35,7 +40,12 @@ const NavOptions = () => {
             />
           </View>
           <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-          <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} type="antdesign" name="arrowright" color="white" />
+          <Icon
+            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+            type="antdesign"
+            name="arrowright"
+            color="white"
+          />
         </TouchableOpacity>
       )}
     />
